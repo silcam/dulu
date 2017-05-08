757701c1a2e51580a5a44e8ec7f5178c56f9841e
Rick Conrad <richardjconrad@gmail.com>
2017-05-08 14:53:38 +0100

    Add name function to Country class. Currently it just connects to english_name, but in the future it should make reference to the current user's preferred UI language

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,14 @@
 class Country < ApplicationRecord
   has_many :languages
+
+  def name
+    #TODO - make this return the appropriate english or french name
+    #depending on the UI Language set for current user
+    self.english_name
+  end
+
+  def name=(name)
+    self.english_name = name
+  end
+
 end
