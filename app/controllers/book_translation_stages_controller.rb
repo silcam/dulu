638022388e3b778f2a75a8de1d085949ac011c2b
Rick Conrad <richardjconrad@gmail.com>
2017-05-10 14:11:09 +0100

    Add create action to BookTranslationStages for updating translation stage of a book

--- app/controllers/book_translation_stages_controller.rb
+++ app/controllers/book_translation_stages_controller.rb
@@ -5,4 +5,18 @@ class BookTranslationStagesController < ApplicationController
     @book_translation_stage = BookTranslationStage.new_for @book_in_translation
   end
 
+  def create
+    @book_in_translation = BookInTranslation.find(params[:book_in_translation_id])
+    if @book_in_translation.book_translation_stages.create(book_translation_stage_params)
+      redirect_to @book_in_translation.project
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def book_translation_stage_params
+      params.require(:book_translation_stage).permit(:translation_stage_id, :start_date)
+    end
+
 end
