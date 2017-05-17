24d9711520612bd318fcf278284a4f739edf9ece
Rick Conrad <richardjconrad@gmail.com>
2017-05-17 12:12:16 +0100

    Initial mock-up of Language Dashboard

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -2,4 +2,8 @@ class LanguagesController < ApplicationController
   def index
     @languages = Language.all.order('name')
   end
+
+  def dashboard
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   resources :people
   resources :organizations
+  get     '/languages/dashboard', to: 'languages#dashboard'
   resources :languages
   shallow do
     resources :programs do
