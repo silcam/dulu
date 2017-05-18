6eb4ed9d0725fd59a3e5e1cd8e5080421ec2992b
Rick Conrad <richardjconrad@gmail.com>
2017-05-18 15:21:57 +0100

    Tweak Rows of Current Activities table to use activity directly

--- app/models/translation_activity.rb
+++ app/models/translation_activity.rb
@@ -13,4 +13,16 @@ class TranslationActivity < ApplicationRecord
     new_book = program.translation_activities.create(bible_book_id: bible_book_id)
     new_book.translation_stages.create(TranslationStage.default_new_params)
   end
+
+  def name
+    self.bible_book.name
+  end
+
+  def progress
+    self.current_translation_stage.progress
+  end
+
+  def stage_name
+    self.current_translation_stage.name
+  end
 end
