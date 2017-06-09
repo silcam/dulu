b5962db7f2d4eabb4896192a463c0f23dd3fd210
Rick Conrad <richardjconrad@gmail.com>
2017-06-09 08:16:14 +0100

    Leave already added people off cbo box for adding people to a program

--- app/models/program.rb
+++ app/models/program.rb
@@ -3,5 +3,13 @@ class Program < ApplicationRecord
   has_many :translation_activities
   has_many :bible_books, through: :translation_activities
   has_many :pers_prog_rels
+  has_many :people, through: :pers_prog_rels
   belongs_to :language
+
+  def unassociated_people
+    excludes = []
+    self.people.each{ |p| excludes << "people.id!=#{p.id}" }
+    excludes.join(' AND ')
+    Person.where(excludes).order(:last_name, :first_name)
+  end
 end
