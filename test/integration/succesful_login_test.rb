ae693f0cb044e72505c12cd05c5cf96c32938fd3
Rick Conrad <richardjconrad@gmail.com>
2017-06-27 14:47:01 +0100

    Login redirect test

--- test/integration/succesful_login_test.rb
+++ test/integration/succesful_login_test.rb
@@ -24,21 +24,9 @@ class SuccesfulLoginTest < Capybara::Rails::TestCase
     assert page.must_have_selector('#error-explanation'), "Expect to see error message for failed log in"
   end
 
-  # test "valid user login and logout" do
-  #   get login_path
-  #   post login_path, params: { session: { email: @jiminy.email}}
-  #   assert_redirected_to root_path
-  #   follow_redirect!
-  #   assert_equal @jiminy, Person.find_by(id: session[:user_id])
-  #   delete logout_path
-  #   assert_redirected_to login_path
-  #   assert_not is_logged_in
-  # end
-  #
-  # test "invalid user login" do
-  #   get login_path
-  #   post login_path, params: { session: {email: @pinochio.email}}
-  #   assert_response :success #means we're still on the login page
-  #   #assert_select  #TODO - check for error message
-  # end
+  test "login redirect" do
+    simulate_oauth @auth_user
+    visit '/organizations'
+    assert_current_path '/organizations'
+  end
 end
