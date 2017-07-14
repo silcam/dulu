d85d27331e3c4209da9659cf21559b5b5751e359
Rick Conrad <richardjconrad@gmail.com>
2017-07-14 12:55:06 +0100

    Sidebar country and region link to list of languages. Languages index has links to active programs

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,6 +1,12 @@
 class LanguagesController < ApplicationController
   def index
-    @languages = Language.all.order('name').includes(:language_status, :country, :cameroon_region)
+    if params[:region]
+      @languages = Language.where(cameroon_region_id: params[:region]).order('name').includes(:language_status, :country, :cameroon_region)
+    elsif params[:country]
+      @languages = Language.where(country_id: params[:country]).order('name').includes(:language_status, :country, :cameroon_region)
+    else
+      @languages = Language.all.order('name').includes(:language_status, :country, :cameroon_region)
+    end
   end
 
   # Create - create a corresponding program
