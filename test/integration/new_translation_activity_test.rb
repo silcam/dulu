c0eb91e0269ab43a099d7faefb8c2472058913ab
Rick Conrad <richardjconrad@gmail.com>
2017-10-18 16:59:12 +0100

    Update tests

--- test/integration/new_translation_activity_test.rb
+++ test/integration/new_translation_activity_test.rb
@@ -7,14 +7,14 @@ class NewTranslationActivityTest < Capybara::Rails::TestCase
     @hdi_program = programs :HdiProgram
     @john = bible_books :John
     log_in @olga
-    visit program_path(@hdi_program)
-    click_link 'Add books to translate'
+    visit program_activities_path(@hdi_program)
+    click_link 'Add Program Activity'
   end
 
   test "Add John Translation" do
     select 'John', from: 'activity_bible_book'
     check 'Drew Maust'
-    click_button 'Add'
+    click_button 'Save'
     assert_current_path dashboard_program_path @hdi_program
     hdi_john = @hdi_program.translation_activities.find_by(bible_book: @john)
     row = find(:css, "tr#activity-row-#{hdi_john.id}")
@@ -26,7 +26,7 @@ class NewTranslationActivityTest < Capybara::Rails::TestCase
   test "Add a whole Testament" do
     select 'New Testament', from: 'activity_bible_book'
     check 'Drew Maust'
-    click_button 'Add'
+    click_button 'Save'
     assert_current_path dashboard_program_path @hdi_program
     click_link 'John'
     assert page.has_content?('Drew Maust'), "Should see Drew's name on the John page"
