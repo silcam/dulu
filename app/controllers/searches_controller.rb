9cc9de723782312aafd486ae7ce5b6a22368c76f
Rick Conrad <richardjconrad@gmail.com>
2018-02-07 10:37:05 +0100

    Prioritize certain models in search results

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,15 +5,20 @@ class SearchesController < ApplicationController
     if(@query.blank?)
       @results = nil
     else
-      @results = @query.blank? ? nil : Program.search(@query) +
-      Activity.search(@query) +
-      Person.search(@query) +
-      Organization.search(@query) +
-      Cluster.search(@query) +
-      Publication.search(@query) +
-      Event.search(@query)
-      if @results.count==1 && @results[0][:model]
+      # Priority search models first
+      @results = Program.search(@query) +
+                 Person.search(@query) +
+                 Organization.search(@query) +
+                 Cluster.search(@query)
+      if @results.count == 1 && @results[0][:model]
         redirect_to @results[0][:model]
+      else
+        @results += Activity.search(@query) +
+                    Publication.search(@query) +
+                    Event.search(@query)
+        if @results.count==1 && @results[0][:model]
+          redirect_to @results[0][:model]
+        end
       end
     end
   end
