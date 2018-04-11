d3911f99a0c7df306386620cd6035101c68f1f6c
Rick Conrad <richardjconrad@gmail.com>
2018-04-11 16:04:31 +0100

    Disable logging in test env to improve test performance

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,5 +38,9 @@ Rails.application.configure do
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  config.action_view.raise_on_missing_translations = true
+
+  # Disable logging to make tests faster
+  config.logger = Logger.new(nil)
+  config.log_level = :fatal
 end
