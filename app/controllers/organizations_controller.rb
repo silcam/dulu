3e386b7534e5abf09150e208954fe423dc2876ba
Rick Conrad <richardjconrad@gmail.com>
2018-05-17 16:33:49 +0100

    Fix edit person link in emails. Do some controller/view cleanup also.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,7 +1,4 @@
 class OrganizationsController < ApplicationController
-  before_action :set_organization, only: [:edit, :update]
-  before_action :authorize_user, only: [:new, :create, :edit, :update]
-
   def index
     @organizations = Organization.all
   end
@@ -10,31 +7,4 @@ class OrganizationsController < ApplicationController
     @organization = Organization.find(params[:id])
     render :index
   end
-
-  # def new
-  #   @organization = Organization.new
-  # end
-  
-  # def create
-  #   @organization = Organization.new(org_params)
-  #   if @organization.save
-  #     redirect_to organizations_path
-  #   else
-  #     render 'new'
-  #   end
-  # end
-
-  # private
-
-  # def org_params
-  #   params.require(:organization).permit(:name, :abbreviation)
-  # end
-
-  # def set_organization
-  #   @organization = Organization.find params[:id]
-  # end
-
-  # def authorize_user
-  #   authorize! :create, Organization
-  # end
 end
