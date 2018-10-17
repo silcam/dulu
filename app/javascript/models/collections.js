1362610b6a083b9d01260f3963a46a2b10ce28b0
Rick Conrad <richardjconrad@gmail.com>
2018-10-17 11:05:09 +0100

    Comment out unused collections.js

--- app/javascript/models/collections.js
+++ app/javascript/models/collections.js
@@ -1,65 +1,73 @@
-import axios from "axios";
-import update from "immutability-helper";
-import { findIndexById, insertInto, deleteFrom } from "../util/findById";
-import { axiosDelete } from "../util/network";
+// Not used...
+// It doens't work because it wants the collection to be passed up front
+// but in practice I want to use the collection that will exist after the
+// network request completes
+// We want to update state with prevState.collection
 
-export default function Collections() {}
+// Could rewrite where it returns an update function ???
 
-Collections.authToken = "";
+// import axios from "axios";
+// import update from "immutability-helper";
+// import { findIndexById, insertInto, deleteFrom } from "../util/findById";
+// import { axiosDelete } from "../util/network";
 
-Collections.index = async type => {
-  const response = await axios.get(baseUrl(type));
-  return response.data[pluralize(type)];
-};
+// export default function Collections() {}
 
-Collections.fetch = async (collection, type, id) => {
-  const response = await axios.get(itemUrl(type, id));
-  return update(collection, {
-    [findIndexById(collection, id)]: { $set: response.data[type] }
-  });
-};
+// Collections.authToken = "";
 
-Collections.add = async (collection, type, item, itemCompare) => {
-  const response = await axios.post(baseUrl(type), {
-    authenticity_token: Collections.authToken,
-    [type]: item
-  });
-  const newItem = response.data[type];
-  return itemCompare
-    ? insertInto(collection, newItem, itemCompare)
-    : update(collection, { $push: [newItem] });
-};
+// Collections.index = async type => {
+//   const response = await axios.get(baseUrl(type));
+//   return response.data[pluralize(type)];
+// };
 
-Collections.update = async (collection, type, item) => {
-  const response = await axios.put(itemUrl(type, item.id), {
-    authenticity_token: Collections.authToken,
-    [type]: item
-  });
-  return update(collection, {
-    [findIndexById(collection, item.id)]: { $set: response.data[type] }
-  });
-};
+// Collections.fetch = async (collection, type, id) => {
+//   const response = await axios.get(itemUrl(type, id));
+//   return update(collection, {
+//     [findIndexById(collection, id)]: { $set: response.data[type] }
+//   });
+// };
 
-Collections.delete = async (collection, type, id) => {
-  const response = await axiosDelete(itemUrl(type, id), {
-    authenticity_token: Collections.authToken
-  });
-  return deleteFrom(collection, id);
-};
+// Collections.add = async (collection, type, item, itemCompare) => {
+//   const response = await axios.post(baseUrl(type), {
+//     authenticity_token: Collections.authToken,
+//     [type]: item
+//   });
+//   const newItem = response.data[type];
+//   return itemCompare
+//     ? insertInto(collection, newItem, itemCompare)
+//     : update(collection, { $push: [newItem] });
+// };
 
-function itemUrl(type, id) {
-  return `${baseUrl(type)}/${id}`;
-}
+// Collections.update = async (collection, type, item) => {
+//   const response = await axios.put(itemUrl(type, item.id), {
+//     authenticity_token: Collections.authToken,
+//     [type]: item
+//   });
+//   return update(collection, {
+//     [findIndexById(collection, item.id)]: { $set: response.data[type] }
+//   });
+// };
 
-function baseUrl(type) {
-  return `/api/${pluralize(type)}`;
-}
+// Collections.delete = async (collection, type, id) => {
+//   const response = await axiosDelete(itemUrl(type, id), {
+//     authenticity_token: Collections.authToken
+//   });
+//   return deleteFrom(collection, id);
+// };
 
-function pluralize(type) {
-  switch (type) {
-    case "person":
-      return "people";
-    default:
-      return type + "s";
-  }
-}
+// function itemUrl(type, id) {
+//   return `${baseUrl(type)}/${id}`;
+// }
+
+// function baseUrl(type) {
+//   return `/api/${pluralize(type)}`;
+// }
+
+// function pluralize(type) {
+//   switch (type) {
+//     case "person":
+//       return "people";
+//     default:
+//       return type + "s";
+//   }
+// }
